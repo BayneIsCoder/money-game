b3b8d3e9d1aefba10d358a289887d9f76caac4a6
BayneIsCoder <baynetv@icloud.com>
2024-11-27 14:34:47 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    info.changeScoreBy(1)
+})
+info.onCountdownEnd(function () {
+    info.changeScoreBy(-2)
+    info.startCountdown(0.5)
+})
+info.onScore(0, function () {
+    game.gameOver(false)
+})
+info.onScore(5, function () {
+    info.stopCountdown()
+    game.showLongText("...", DialogLayout.Bottom)
+    game.gameOver(true)
+})
+game.splash("SoftArcade Presents..")
+game.splash("money game")
+game.showLongText("You accidentally signed up for a subscription that costs 2usd every second.", DialogLayout.Full)
+game.showLongText("To Cancel The Subscription, Get To 500usd.", DialogLayout.Full)
+game.showLongText("your job is to get lots and lots of money.", DialogLayout.Full)
+game.splash("Click B Button To Earn Money!")
+info.startCountdown(5)
